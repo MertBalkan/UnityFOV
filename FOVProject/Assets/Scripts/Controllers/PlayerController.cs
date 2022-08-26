0febb02c556727ad72c939487f31cecac93df977
Mert Balkan <mert__balkan@hotmail.com>
2022-08-26 21:22:09 +0300

    FOV Settings added

--- FOVProject/Assets/Scripts/Controllers/PlayerController.cs
+++ FOVProject/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,29 +4,20 @@ using System.Collections.Generic;
 using System.Numerics;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 using Vector3 = UnityEngine.Vector3;
 
 namespace fov
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private Vector3 lineVector1;
-        [SerializeField] private Vector3 lineVector2;
-        [Range(-1f, 0)]
-        [SerializeField] private float fovValue = -0.41f;
-        
-        [SerializeField] private float playerRadius;
-        
         private RaycastHit _hit;
         private Vector3 _mousePos;
         private Vector3 _direction;
 
-        // private TargetController[] _targets;
-
         private void Update()
         {
             PlayerLook();
-            ViewAngle();
         }
 
         private void PlayerLook()
@@ -41,68 +32,6 @@ namespace fov
                 this.transform.LookAt(_hit.point);
                 transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, transform.localEulerAngles.z);
             }
-        }
-
-        private void FindTargets()
-        {
-            
-        }
-
-        private void ViewAngle()
-        {
-        }
-
-#if UNITY_EDITOR
-        
-        private void OnDrawGizmos()
-        {
-            lineVector1.x = fovValue;
-            lineVector2.x = -fovValue;
-            Handles.DrawWireDisc(transform.position, transform.up, playerRadius);
-
-            var playerPos = transform.localPosition;
-            
-            var linePos = lineVector1 * playerRadius;
-            Handles.color = Color.magenta;
-            var forwardVector = (playerPos + linePos);
-            Handles.DrawLine(playerPos, forwardVector, 5.0f);
-            float angle = Mathf.Atan2(linePos.y, linePos.x) * Mathf.Rad2Deg;
-            Debug.Log("Angle: " + angle);
-            lineVector1.z = Mathf.Sqrt(Mathf.Pow(playerRadius, 2) - Mathf.Pow(linePos.x, 2)) / playerRadius;
-            
-            var linePos2 = lineVector2 * playerRadius;
-            Handles.color = Color.blue;
-            var forwardVector2 = (playerPos + linePos2);
-            Handles.DrawLine(playerPos, forwardVector2, 5.0f);
-            float angle2 = Mathf.Atan2(linePos2.y, linePos2.x) * Mathf.Rad2Deg;
-            Debug.Log("Angle: " + angle2);
-            lineVector2.z = Mathf.Sqrt(Mathf.Pow(playerRadius, 2) - Mathf.Pow(linePos2.x, 2)) / playerRadius;
-
-            var dotProduct = Vector3.Dot(lineVector1, lineVector2);
-
-            float angleRadian = Mathf.Acos(dotProduct);
-
-            Handles.DrawWireArc(transform.position, Vector3.up, lineVector1, angleRadian * Mathf.Rad2Deg, 1.0f, 2.0f);
-
-            Handles.color = Color.white;
-        }
-#endif
-        private void CalculateLine(Vector3 line)
-        {
-            var playerPos = transform.localPosition;
-            
-            var linePos = line * playerRadius;
-            
-            Handles.color = Color.magenta; 
-            
-            var forwardVector = (playerPos + linePos);
-            
-            Handles.DrawLine(playerPos, forwardVector, 5.0f);
-            
-            float angle = Mathf.Atan2(linePos.y, linePos.x) * Mathf.Rad2Deg;
-            Debug.Log("Angle: " + angle);
-            
-            line.z = Mathf.Sqrt(Mathf.Pow(playerRadius, 2) - Mathf.Pow(linePos.x, 2)) / playerRadius;
         }
     }
 }
---
+++ FOVProject/Assets/Scripts/Settings/FOVSettings.cs
@@ -0,0 +1,68 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace fov
+{
+    public class FOVSettings : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct FOVStruct
+        {
+            [Range(-1f, 0f)] 
+            public float fovValue;
+            public float playerRadius;
+            public LineVector lineVector;
+            public GameObject player;
+
+        }
+
+        [System.Serializable]
+        public struct LineVector
+        {
+            public Vector3 lineVector1;
+            public Vector3 lineVector2;
+        }
+            
+        [SerializeField] private FOVStruct fovSettings;
+        
+        
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            SetFOV();
+            Handles.color = Color.white;
+            Handles.DrawWireDisc(fovSettings.player.transform.position, transform.up, fovSettings.playerRadius);
+
+            var playerPos = fovSettings.player.transform.localPosition;
+            
+            var linePos = fovSettings.lineVector.lineVector1 * fovSettings.playerRadius;
+            Handles.color = Color.green;
+            var forwardVector = (playerPos + linePos);
+            Handles.DrawLine(playerPos, forwardVector, 5.0f);
+            
+            fovSettings.lineVector.lineVector1.z = Mathf.Sqrt(Mathf.Pow(fovSettings.playerRadius, 2) - Mathf.Pow(linePos.x, 2)) / fovSettings.playerRadius;
+            
+            var linePos2 =  fovSettings.lineVector.lineVector2 * fovSettings.playerRadius;
+            Handles.color = Color.red;
+            var forwardVector2 = (playerPos + linePos2);
+            Handles.DrawLine(playerPos, forwardVector2, 5.0f);
+            fovSettings.lineVector.lineVector2.z = Mathf.Sqrt(Mathf.Pow(fovSettings.playerRadius, 2) - Mathf.Pow(linePos2.x, 2)) / fovSettings.playerRadius;
+
+            var dotProduct = Vector3.Dot( fovSettings.lineVector.lineVector1,  fovSettings.lineVector.lineVector2);
+
+            var angleRadian = Mathf.Acos(dotProduct);
+            
+            Handles.Label(playerPos + Vector3.forward * 0.8f, (angleRadian * Mathf.Rad2Deg).ToString());
+
+            Handles.color = Color.yellow;
+            Handles.DrawWireArc(playerPos, Vector3.up,  fovSettings.lineVector.lineVector1, angleRadian * Mathf.Rad2Deg, 1.0f, 2.0f);
+            Handles.color = Color.white;
+        }
+#endif
+        private void SetFOV()
+        {
+            fovSettings.lineVector.lineVector1.x =  fovSettings.fovValue;
+            fovSettings.lineVector.lineVector2.x = -fovSettings.fovValue;
+        }
+    }
+}
